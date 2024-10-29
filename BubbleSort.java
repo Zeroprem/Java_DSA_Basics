8d20ee371953331351e611ab63bb002b7fa705ae
Premchand Tarange <premchandtarange.sknsits.comp@gmail.com>
2024-10-29 09:21:07 +0530

    Bubble sort updated

--- BubbleSort.java
+++ BubbleSort.java
@@ -25,27 +25,11 @@ public class BubbleSort {
 				}
 			}
 		}
-	 static void insertionSorting(int[] arr) {
-			
-			
-	        int n = arr.length;
-
-	        for (int i = 1; i < n; ++i) {
-	            int cur = arr[i];
-	            int j = i - 1;
-
-	            while (j >= 0 && arr[j] > cur) {
-	                arr[j + 1] = arr[j];
-	               j--;
-	            }
-	            arr[j + 1] = cur;	        }
-
-
-	    }
+	 
 	public static void main(String[] args) {
 		int [] ar= {6,5,4,3,2,1};
 		bubbleSort(ar);
-		insertionSorting(ar);
+		
 		display(ar);
 
 	}
